8faa8a52f3cd56712bfd5189ca7d08c4502745e7
WinZip <wydo@protonmail.com>
2019-11-21 10:07:03 +0300

    Updates

---
+++ precache-manifest.1127dea8d8f96ddfbeb5c83e36aa0d4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920f0ecc32c4e06929368045c8f3bffd",
+    "url": "/vk-wheel/index.html"
+  },
+  {
+    "revision": "59cda34d7115819867d7",
+    "url": "/vk-wheel/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "612ce75c0ec094f3424d",
+    "url": "/vk-wheel/static/css/main.92af9383.chunk.css"
+  },
+  {
+    "revision": "59cda34d7115819867d7",
+    "url": "/vk-wheel/static/js/2.6595a718.chunk.js"
+  },
+  {
+    "revision": "612ce75c0ec094f3424d",
+    "url": "/vk-wheel/static/js/main.40a88545.chunk.js"
+  },
+  {
+    "revision": "b90f2bb12fd1a2521e1f",
+    "url": "/vk-wheel/static/js/runtime-main.cdf45c8b.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-wheel/static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fbfaa946f4f0f2e1c33e3071b4935a1b.js"
+  "/vk-wheel/precache-manifest.1127dea8d8f96ddfbeb5c83e36aa0d4d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vk-wheel/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
